6a78640017dfffbb7fc88ec58f78bb5fe2485d8c
euglazer <eugenestapler@gmail.com>
2016-07-01 16:35:20 -0400

    tests for add/remove/increment/decrement counter reducers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import deepFreeze from 'deep-freeze'
+import expect from 'expect'
 
 // reducer
 const counter = (state = 0, action) => {
@@ -46,3 +48,87 @@ const render = () => {
 
 store.subscribe(render)
 render()
+
+// -----------------------------
+
+const addCounter = (list) => {
+  return [...list, 0]
+}
+
+const removeCounter = (list, i) => {
+  return [
+    ...list.slice(0, i),
+    ...list.slice(i + 1)
+  ]
+}
+
+const incrementCounter = (list, i) => {
+  return [
+    ...list.slice(0, i),
+    list[i] + 1,
+    ...list.slice(i + 1)
+  ]
+}
+
+const decrementCounter = (list, i) => {
+  return [
+    ...list.slice(0, i),
+    list[i] - 1,
+    ...list.slice(i + 1)
+  ]
+}
+
+const testAddCounter = () => {
+  const listBefore = []
+  const listAfter = [0]
+
+  deepFreeze(listBefore)
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter)
+
+  console.log('addCounter works!')
+}
+testAddCounter()
+
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20]
+  const listAfter = [0, 20]
+
+  deepFreeze(listBefore)
+
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter)
+
+  console.log('removeCounter works!')
+}
+testRemoveCounter()
+
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20]
+  const listAfter = [0, 11, 20]
+
+  deepFreeze(listBefore)
+
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter)
+
+  console.log('incrementCounter works!')
+}
+testIncrementCounter()
+
+const testDecrementCounter = () => {
+  const listBefore = [0, 10, 20]
+  const listAfter = [0, 9, 20]
+
+  deepFreeze(listBefore)
+
+  expect(
+    decrementCounter(listBefore, 1)
+  ).toEqual(listAfter)
+
+  console.log('decrementCounter works!')
+}
+testDecrementCounter()
