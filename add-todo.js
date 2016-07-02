cbb5b6c4ae68b23879ba79a38c487aa570f98657
euglazer <eugenestapler@gmail.com>
2016-07-02 13:53:22 -0400

    use connect to turn AddTodo into a container component

--- add-todo.js
+++ add-todo.js
@@ -1,23 +1,22 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 let nextTodoId = 0
-const AddTodo = (props, {store}) => {
-  let input
+const AddTodo = connect()( // does not subscribe to store, but passes dispatch
+  ({dispatch}) => {
+    let input
 
-  return (
-    <div>
-      <input ref={node => { input = node }} />
+    return (
+      <div>
+        <input ref={node => { input = node }} />
 
-      <button onClick={() => {
-        store.dispatch({ type: 'ADD_TODO', text: input.value, id: nextTodoId++})
-        input.value = ''
-      }}>Add Todo</button>
-    </div>
-  )
-}
-
-AddTodo.contextTypes = {
-  store: React.PropTypes.object
-}
+        <button onClick={() => {
+          dispatch({ type: 'ADD_TODO', text: input.value, id: nextTodoId++})
+          input.value = ''
+        }}>Add Todo</button>
+      </div>
+    )
+  }
+)
 
 export default AddTodo
