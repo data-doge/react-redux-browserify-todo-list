3d54f0b710360f7512ea821c31376dd1308646b6
euglazer <eugenestapler@gmail.com>
2016-07-02 16:48:37 -0400

    save state to localStorage on state change, at at most once a second

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,6 @@
 import * as types from '../constants/action-types'
+import v4 from 'node-uuid'
 
-let nextId = 0
-export const addTodo = (text) => ({type: types.ADD_TODO, text, id: nextId++})
+export const addTodo = (text) => ({type: types.ADD_TODO, text, id: v4()})
 export const toggleTodo = (id) => ({type: types.TOGGLE_TODO, id})
 export const setVisibilityFilter = (filter) => ({type: types.SET_VISIBILITY_FILTER, filter})
--- index.js
+++ index.js
@@ -4,7 +4,17 @@ import {render} from 'react-dom'
 import rootReducer from './reducers'
 import App from './components'
 import {createStore} from 'redux'
-const store = createStore(rootReducer)
+import { loadState, saveState } from './local-storage'
+import throttle from 'lodash.throttle'
+
+const persistedState = loadState()
+const store = createStore(rootReducer, persistedState)
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos
+  })
+}), 1000)
 
 render(
   <Provider store={store}>
