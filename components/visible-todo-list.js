64a307819983e8fb92a0ea700b1957a47dc1bde7
euglazer <eugenestapler@gmail.com>
2016-07-23 12:10:44 -0400

    separate visible-todo-list child components into their own files

--- components/visible-todo-list.js
+++ components/visible-todo-list.js
@@ -1,30 +1,9 @@
 import React from 'react'
-const {Component} = React
 import {connect} from 'react-redux'
 import {toggleTodo} from '../actions'
 import {withRouter} from 'react-router'
+import TodoList from './todo-list'
 import {getVisibleTodos} from '../reducers'
-
-const Todo = ({onClick, completed, text}) => (
-  <li
-    onClick={onClick}
-    style={{textDecoration: completed ? 'line-through' : 'none'}}
-  >
-    {text}
-  </li>
-)
-
-const TodoList = ({todos, onTodoClick}) => (
-  <ul>
-    {todos.map(todo =>
-      <Todo
-        key={todo.id}
-        {...todo}
-        onClick={() => onTodoClick(todo.id)}
-      />
-    )}
-  </ul>
-)
 
 const mapStateToProps = (state, {params}) => ({
   todos: getVisibleTodos(state, params.filter || 'all')
