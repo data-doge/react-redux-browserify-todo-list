bfee1beec27bc860543ca7b92bb5b43f519d6212
euglazer <eugenestapler@gmail.com>
2016-07-23 14:58:54 -0400

    wrapping dispatch to recognize promises

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,18 @@
 import * as types from '../constants/action-types'
 import v4 from 'node-uuid'
+import * as api from '../api'
+
+const receiveTodos = (filter, response) => ({
+  type: 'RECEIVE_TODOS',
+  filter,
+  response
+})
+
+export const fetchTodos = (filter) => {
+  return api.fetchTodos(filter).then((response) => {
+    return receiveTodos(filter, response)
+  })
+}
 
 export const addTodo = (text) => ({
   type: types.ADD_TODO,
@@ -10,10 +23,4 @@ export const addTodo = (text) => ({
 export const toggleTodo = (id) => ({
   type: types.TOGGLE_TODO,
   id
-})
-
-export const receiveTodos = (filter, response) => ({
-  type: 'RECEIVE_TODOS',
-  filter,
-  response
 })
