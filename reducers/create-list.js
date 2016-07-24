21048afc525b86a37ccc234d384c49b6af189098
euglazer <eugenestapler@gmail.com>
2016-07-24 17:07:52 -0400

    updating data on the server

--- reducers/create-list.js
+++ reducers/create-list.js
@@ -1,7 +1,19 @@
-import {FETCH_TODOS_REQUEST, FETCH_TODOS_SUCCESS, FETCH_TODOS_FAILURE, ADD_TODO_SUCCESS} from '../constants/action-types'
+import {FETCH_TODOS_REQUEST, FETCH_TODOS_SUCCESS, FETCH_TODOS_FAILURE, ADD_TODO_SUCCESS, TOGGLE_TODO_SUCCESS} from '../constants/action-types'
 import {combineReducers} from 'redux'
 
 const createList = (filter) => {
+  const handleToggle = (state, action) => {
+    const {result: toggledId, entities} = action.response
+    const {completed} = entities.todos[toggledId]
+    const shouldRemove = (
+      (completed && filter === 'active') ||
+      (!completed && filter === 'completed')
+    )
+    return shouldRemove ?
+      state.filter(id => id !== toggledId) :
+      state
+  }
+
   const ids = (state = [], action) => {
     switch (action.type) {
       case FETCH_TODOS_SUCCESS:
@@ -12,6 +24,8 @@ const createList = (filter) => {
         return filter !== 'completed' ?
           [...state, action.response.result] :
           state
+      case TOGGLE_TODO_SUCCESS:
+        return handleToggle(state, action)
       default:
         return state
     }
