435038b4e8a761145d011e2793900a62be9e6cc5
euglazer <eugenestapler@gmail.com>
2016-07-23 20:58:35 -0400

    avoiding race conditions with thunks

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,6 @@
 import * as types from '../constants/action-types'
 import v4 from 'node-uuid'
+import {getIsFetching} from '../reducers'
 import * as api from '../api'
 
 const requestTodos = (filter) => ({
@@ -13,9 +14,13 @@ const receiveTodos = (filter, response) => ({
   response
 })
 
-export const fetchTodos = (filter) => (dispatch) => {
+export const fetchTodos = (filter) => (dispatch, getState) => {
+  if (getIsFetching(getState(), filter))
+    return Promise.resolve()
+
   dispatch(requestTodos(filter))
-  return api.fetchTodos(filter).then((response) => {
+
+  return api.fetchTodos(filter).then(response => {
     dispatch(receiveTodos(filter, response))
   })
 }
